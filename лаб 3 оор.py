def5bb395afb70a4d1718c0b4be7cf41d52f434d
MartaSniadankoKN <marta.sniadanko.kn.2023@lpnu.ua>
2025-05-19 18:51:55 +0300

    Add files via upload

---
+++ лаб 3 оор.py
@@ -0,0 +1,20 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/S.Marta/AppData/Local/Programs/Python/Python313/лаб 3. оор.py
+=== Original ===
+Title: Original Document
+Content: This is the content of the original document.
+
+=== Clone 1 ===
+Title: Cloned Document 1
+Content: This is the content of the first clone.
+
+=== Clone 2 ===
+Title: Cloned Document 2
+Content: This is the content of the second clone.
+
+=== Original Again (Unchanged) ===
+Title: Original Document
+Content: This is the content of the original document.
+
